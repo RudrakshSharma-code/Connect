89d6819eb3f1bedac577f42a02b50912d5078f36
Vyacheslav Zhilenkov <slavazhil@gmail.com>
2020-04-29 22:43:11 -0700

    add aws functions

--- src/js/aws.js
+++ src/js/aws.js
@@ -1,44 +1,131 @@
 ////////////////////////////////////////////////////////////////////////////////////
-import API, { graphqlOperation } from '@aws-amplify/api'
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+import { API, graphqlOperation } from '@aws-amplify/api'
 import PubSub from '@aws-amplify/pubsub';
-import { createPost } from '../graphql/mutations'
-import { listPosts } from '../graphql/queries'
-import { onCreatePost } from '../graphql/subscriptions'
+import * as mutations from '../graphql/mutations'
+import * as queries from '../graphql/queries';
+import * as subscriptions from '../graphql/subscriptions'
 import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from '../aws-exports';
 API.configure(awsconfig);
 PubSub.configure(awsconfig);
 Amplify.configure(awsconfig);
 ////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
 
-async function createNewPost(user, title, description) {
+async function createPost(user, title, description) {
   const post = {"user": user, "title": title, "description": description};
-  return await API.graphql(graphqlOperation(createPost, { "input": post }))
+  const answer = await API.graphql(graphqlOperation(mutations.createPost, { "input": post, "condition": null }));
+  return answer.data.createPost;
 }
 
-
-API.graphql(graphqlOperation(onCreatePost)).subscribe({
-  next: (evt) =>{
-    const post = evt.value.data.onCreatePost;
-    console.log(post);
-  }
-});
-
-async function getAllPosts() {
-  API.graphql(graphqlOperation(listPosts)).then((evt) => {
-    evt.data.listPosts.items.map((post, i) => console.log(post));
-  })
+async function updatePost(id, title, description) {
+  const post = {"id": id, "title": title, "description": description};
+  const answer = await API.graphql(graphqlOperation(mutations.updatePost, { "input": post, "condition": null }));
+  return answer.data.updatePost;
 }
 
+async function deletePost(id) {
+  const post = {"id": id};
+  const answer = await API.graphql(graphqlOperation(mutations.deletePost, {"input": post, "condition": null }));
+  return answer.data.deletePost;
+}
 
-async function signUp(email, password, phone_number) {
+async function getPost(id) {
+  const answer = await API.graphql(graphqlOperation(queries.getPost, {"id": id}));
+  return answer.data.getPost;
+}
+
+async function listPosts(filter) {
+  // filter is optional
+  // example
+  // filter = {user: {eq: "userID"}};
+  const limit = 50; // what this limit should be?
+  let posts = [];
+  let answer;
+  let token;
+
+  do {
+    answer = await API.graphql(graphqlOperation(queries.listPosts, {"filter": filter, "limit": limit, "nextToken": token}));
+    posts = posts.concat(answer.data.listPosts.items);
+    token = answer.data.listPosts.nextToken;
+  } while (token);
+
+  return posts;
+}
+
+function onCreatePost() {
+  const listener = API.graphql(graphqlOperation(subscriptions.onCreatePost))
+    .subscribe({next: (postData) => {
+      console.log("On create post: ", postData.value.data.onCreatePost);
+  }});
+  return listener;
+}
+
+function onUpdatePost() {
+  const listener = API.graphql(graphqlOperation(subscriptions.onUpdatePost))
+    .subscribe({next: (postData) => {
+      console.log("On update post: ", postData.value.data.onUpdatePost);
+  }});
+  return listener;
+}
+
+function onDeletePost() {
+  const listener = API.graphql(graphqlOperation(subscriptions.onDeletePost))
+    .subscribe({next: (postData) => {
+      console.log("On delete post: ", postData.value.data.onDeletePost);
+  }});
+  return listener;
+}
+
+function stopListener(listener) {
+  listener.unsubscribe();
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// function testPosts() {
+//   onCreatePost();
+//   onUpdatePost();
+//   onDeletePost();
+
+//   setTimeout(async () => {
+//     let answer = await createPost("user12345", "title12345", "description12345");
+//     console.log("Create post: ", answer);
+  
+//     answer = await updatePost(answer.id, "titleUpdated", "descriptionUpdated");
+//     console.log("Update post: ", answer);
+  
+//     answer = await getPost(answer.id);
+//     console.log("Get post: ", answer);
+  
+//     answer = await deletePost(answer.id);
+//     console.log("Delete post: ", answer);
+  
+//     answer = await listPosts();
+//     console.log("List posts: ", answer);
+//   }, 1000); // wait 1 second
+
+// }
+
+// testPosts();
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+export async function signUp(email, password, phone_number, given_name, family_name) {
   try {
       const user = await Auth.signUp({
           "username": email,
           "password": password,
           "attributes": {
-              "phone_number": phone_number // "+18882223344"
-              // other custom attributes
+            "given_name": given_name,
+            "family_name": family_name,
+            "phone_number": phone_number // "+18882223344"
           }
       });
       console.log({ user });
@@ -47,8 +134,17 @@ async function signUp(email, password, phone_number) {
   }
 }
 
+async function resendConfirmationCode(email) {
+  try {
+      await Auth.resendSignUp(email);
+      console.log('code resent succesfully');
+  } catch (error) {
+      console.log('error resending code: ', error);
+  }
+}
 
-async function confirmSignUp(email, code) {
+
+export async function confirmSignUp(email, code) {
   try {
     const user = await Auth.confirmSignUp(email, code);
     console.log({ user });
@@ -57,7 +153,7 @@ async function confirmSignUp(email, code) {
   }
 }
 
-async function signIn(email, password) {
+export async function signIn(email, password) {
   try {
       const user = await Auth.signIn(email, password);
       console.log({ user });
@@ -73,4 +169,40 @@ async function signOut() {
   } catch (error) {
       console.log('error signing out: ', error);
   }
+}
+
+async function changePassword(oldPassword, newPassword) {
+  Auth.currentAuthenticatedUser()
+  .then(user => {
+      return Auth.changePassword(user, oldPassword, newPassword);
+  })
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+}
+
+async function forgotPassword(username) {
+  Auth.forgotPassword(username)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+}
+
+async function forgotPasswordSubmit(username, code, new_password) {
+  Auth.forgotPasswordSubmit(username, code, new_password)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+}
+
+
+async function currentAuthenticatedUser() {
+  Auth.currentAuthenticatedUser({
+    bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+  }).then(user => console.log(user))
+  .catch(err => console.log(err));
+}
+
+async function updateUserAttributes(user, given_name, family_name) {
+  await Auth.updateUserAttributes(user, {
+    'given_name': given_name,
+    'family_name': family_name
+  });
 }
--- src/js/test.js
+++ src/js/test.js
@@ -1,3 +1,34 @@
-import "./aws.js";
+import * as aws from "./aws.js";
 
-console.log("TEST.JS");
+let signUpButton = document.getElementById("signUpButton");
+signUpButton.addEventListener("click", signUp);
+
+function signUp() {
+    aws.signUp(
+        document.getElementById("signUpEmail").value, 
+        document.getElementById("signUpPassword").value,
+        "+18882223344",
+        "given_name", 
+        "family_name"
+    );
+}
+
+function confirmEmail() {
+    aws.confirmSignUp(
+        document.getElementById("confirmEmail").value, 
+        document.getElementById("confirmCode").value
+    )
+}
+
+let confirmButton = document.getElementById("confirmButton");
+confirmButton.addEventListener("click", confirmEmail);
+
+function signIn() {
+    aws.signIn(
+        document.getElementById("signInEmail").value, 
+        document.getElementById("signInPassword").value,
+    )
+    
+}
+let signInButton = document.getElementById("signInButton");
+signInButton.addEventListener("click", signIn);
