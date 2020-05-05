04678d0f14720fd61d0a8ff0dc187011acb6f0e5
Vitoria Postai Martins <49215543+vitoriapostaimartins@users.noreply.github.com>
2020-05-05 10:25:12 -0700

    added map to requests page

--- src/js/requests.js
+++ src/js/requests.js
@@ -27,8 +27,87 @@ async function showPosts(){
     }
 }
 
-function handle(){
+function works(x, y) {
+    var mymap = L.map("mapid").setView([x, y], 13);
+    L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+          "pk.eyJ1Ijoidml0b3JpYXBvc3RhaW1hcnRpbnMiLCJhIjoiY2s5a2llYXM5MDZxaDNvbWt0YWd4NXE5NyJ9.4gJv-_McQLbJg3Gn4vUl7g",
+      }
+    ).addTo(mymap);
+
+    function onPopupClick() {
+      var data = this._popup._content;
+      alert("You clicked the map at " + $(data).text());
+  }
+
+  var desc = ["<b>Ana Banana</b><br>Oranges needed", 
+              "<b>Vitoria Martins</b><br>Groceries needed   ", 
+              "<b>Jeremy </b><br>Need Food   ",
+              "<b>Rudy</b><br>Someone help   ",
+              "<b>Slavat</b><br>Out of Tylenol   "]
+
+  // let i = 0;
+  // desc.forEach(mes=>function(){
+  //   var marker = new L.marker([(x + (-0.3)**i), (y - (-0.3)**i)]).addTo(mymap);
+  //   marker
+  //     .bindPopup(mes)
+  //     // .bindPopup("Youre here!")
+  //     .openPopup();
+  //     marker.on('click', onPopupClick);
+  //     i++;
+  // })
+
+  for (var i = 0; i < desc.length; i++) {
+    var marker = new L.marker([(x + (-0.3)**i), (y - (-0.3)**i)]).addTo(mymap);
+    marker
+      .bindPopup(desc[i]);
+      marker.on('click', onPopupClick);
+  }
+
+  var circle = L.circle([x, y], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 1500
+}).addTo(mymap);
+    
+
+    // var marker = new L.marker([49.2827, 123.1207]).addTo(mymap);
+    // marker
+    //   .bindPopup("<b>First Last</b><br>Food needed   ")
+    //   // .bindPopup("Youre here!")
+    //   .openPopup();
+    //   marker.on('click', onPopupClick);
+    
+    
+    
+  }
+
+ function setMap(){
+    if (window.navigator.geolocation) {
+        // Geolocation available
+        window.navigator.geolocation.getCurrentPosition(
+          function (result) {
+            works(result.coords.latitude, result.coords.longitude);
+          },
+          console.log("")
+        );
+      }
+  }
+
+$(document).ready(function(){
+    setMap();
+    console.log("asking for map");
 }
+);
 showPosts();
 
 
