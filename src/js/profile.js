74d67144c8a26c96e97897711780fec0419a5e22
Vitoria Postai Martins <49215543+vitoriapostaimartins@users.noreply.github.com>
2020-05-13 15:25:05 -0700

    fixed map

--- src/js/profile.js
+++ src/js/profile.js
@@ -18,12 +18,60 @@ function setButton(){
   })
 }
 
+// MAP
+
+var mymap
+
+async function setMap() {
+  mymap = (mymap = L.map("mapid"));
+  let u = await user;
+  var ulatitude = u.attributes["custom:latitude"];
+  var ulongitude = u.attributes["custom:longitude"];
+  works(ulatitude, ulongitude);
+}
+
+function works(x, y) {
+  buildMap(x, y);
+  addMarker(x, y);
+}
+
+
+function buildMap(x, y) {
+  mymap.setView([x, y], 13);
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken:
+        "pk.eyJ1Ijoidml0b3JpYXBvc3RhaW1hcnRpbnMiLCJhIjoiY2s5a2llYXM5MDZxaDNvbWt0YWd4NXE5NyJ9.4gJv-_McQLbJg3Gn4vUl7g",
+    }
+  ).addTo(mymap);
+}
+
+function addMarker(x, y){
+  var marker = new L.marker([x, y]).addTo(mymap)
+}
+
+
+
+
+//DELETTE
+
 async function deletePost(id) {
   return await aws.deletePost(id);
 }
 
+//START
+
 $(document).ready(function(){
   changeHtml();
+  setMap();
   setButton();
 })
+
 
