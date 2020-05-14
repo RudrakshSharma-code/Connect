91c837ed371490b649fbf2c13dcc359169653180
Jeremy Schmidt <JeremyschmidtID@gmail.com>
2020-05-14 14:35:36 -0700

    info map is working

--- src/js/info.js
+++ src/js/info.js
@@ -36,6 +36,36 @@ async function run() {
     post = await aws.getPost(id);
     setVars();
     editHtml();
+    setMap();
 }
+
+//map set-up
+async function works(x, y) {
+    const user = await aws.currentAuthenticatedUser();
+    var mymap = L.map("mapid").setView([x, y], 13);
+    L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors,' +
+          ' <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © ' +
+          '<a href="https://www.mapbox.com/">Mapbox</a>, Powered by <a href="https://www.esri.com/">Esri</a>',
+        maxZoom: 18,
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: "pk.eyJ1Ijoidml0b3JpYXBvc3RhaW1hcnRpbnMiLCJhIjoiY2s5a2llYXM5MDZxaDNvbWt0YWd4NXE5NyJ9.4gJv-_McQLbJg3Gn4vUl7g",
+      }
+    ).addTo(mymap);
+  
+    var marker = L.marker([x, y]).addTo(mymap);
+  }
+  
+  async function setMap() {
+    const user = await aws.currentAuthenticatedUser();
+    console.log(post);
+    var ulatitude = post.latitude;
+    var ulongitude = post.longitude;
+    works(ulatitude, ulongitude);
+  }
+  
 
 run();
